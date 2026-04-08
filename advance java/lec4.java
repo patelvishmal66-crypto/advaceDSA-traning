531465c7193b88981b2d64fbc4b61b991914ae4d
patelvishmal66-crypto <patelvishmal66@gmail.com>
2026-04-08 11:17:11 +0530

    lec4.java

--- advance java/lec4.java
+++ advance java/lec4.java
@@ -1,7 +1,53 @@
-public class main 
-{
+// public class lec4 {
+//     public static void main(String[] args) {
+//         String str1 = "hello";
+//         String str2 = new String("hi");
+//         System.out.println(str1);
+//         System.out.println(str2);
+//         System.out.println(str1.length());
+//         System.out.println(str2.substring(0, 2));
+//         System.out.println(str1.replace('l', 'z'));
+//            System.out.println(str1.toUpperCase());
+//            System.out.println(str1.toLowerCase());
+//            System.out.println(str1.charAt(2));
+//            System.out.println(str1.equals(str2));
+//            System.out.println(str1.contains("world"));
+//            String rev = "";
+//            for (int i = str1.length() - 1; i >= 0; i--) {
+//                rev = rev + str1.charAt(i);
+//            }
+//            System.out.println(rev);
+//            if(str1.equals(rev))
+//            {
+//                System.out.println("palindrome");
+//            }
+//            else
+//            {
+//                System.out.println("not palindrome");
+//            }
+//     }
+// }
+import java.util.Arrays;
+
+public class lec4{
     public static void main(String[] args) {
-        string str1 = "hello";
-        string str2 = 
+        String str1 = "silent";
+        String str2 = "listen";
+
+        char[] a  = str1.toCharArray();
+        char[] b = str2.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        if(Arrays.equals(a, b))
+        {
+            System.out.println("anagram");
+        }
+        else
+        {
+            System.out.println("not anagram");
+        }
     }
 }
+
